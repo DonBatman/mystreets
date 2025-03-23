01797e6fa8d16760e7f510a25e18042211c24a68
Don <serfdon@gmail.com>
2025-03-22 21:05:10 -0700

    Another item

--- traffic_cone.lua
+++ traffic_cone.lua
@@ -33,3 +33,52 @@ minetest.register_node("mystreets:traffic_cone", {
 		}
 	},
 })
+--Craft
+minetest.register_craft({
+	output = "mystreets:traffic_cone 2",
+	recipe = {
+		{"","dye:orange",""},
+		{"","mystreets:plastic",""},
+		{"mystreets:plastic","mystreets:plastic","mystreets:plastic"},
+	}
+})
+minetest.register_node("mystreets:pylon", {
+	description = "Pylon",
+	tiles = {
+		"mystreets_traffic_cone_top.png",
+		"mystreets_traffic_cone_bottom.png",
+		"mystreets_traffic_cone_side.png",
+		"mystreets_traffic_cone_side.png",
+		"mystreets_traffic_cone_side.png",
+		"mystreets_traffic_cone_side.png",
+	},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	groups = {cracky = 2},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.1875, -0.5, -0.125, 0.1875, -0.4375, 0.125},
+			{-0.0625, -0.5, -0.0625, 0.0625, 0.5, 0.0625},
+			{-0.125, -0.5, -0.1875, 0.125, -0.4375, 0.1875},
+		}
+	},
+	selection_box = {
+		type = "fixed",
+		fixed = {
+			{-0.1875, -0.5, -0.125, 0.1875, -0.4375, 0.125},
+			{-0.0625, -0.5, -0.0625, 0.0625, 0.5, 0.0625},
+			{-0.125, -0.5, -0.1875, 0.125, -0.4375, 0.1875},
+		}
+	},
+})
+--Craft
+minetest.register_craft({
+	output = "mystreets:pylon 2",
+	recipe = {
+		{"","mystreets:plastic",""},
+		{"","mystreets:plastic","dye:orange"},
+		{"","mystreets:plastic",""},
+	}
+})
