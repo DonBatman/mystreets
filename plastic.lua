026e5c2cecaca53ea88954557ebd18e797539b58
Don <serfdon@gmail.com>
2025-03-26 12:52:11 -0700

    added plastic recipe

--- plastic.lua
+++ plastic.lua
@@ -2,3 +2,10 @@ core.register_craftitem("mystreets:plastic", {
     description = "Plastic",
     inventory_image = "mystreets_plastic.png"
 })
+--Craft
+minetest.register_craft({
+		type = "cooking",
+		output = "mystreets:plastic 20",
+		recipe = "mystreets:tar",
+		cooktime = 5
+	})
